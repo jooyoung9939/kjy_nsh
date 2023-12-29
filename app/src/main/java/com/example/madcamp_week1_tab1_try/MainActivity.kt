14f76b23ff62d34873ce7861041d0d27cc4bef83
hello0821 <hellonsh0821@gmail.com>
2023-12-29 10:56:42 +0900

    231229 ㅜnew test

---
+++ app/src/main/java/com/example/madcamp_week1_tab1_try/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madcamp_week1_tab1_try
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
